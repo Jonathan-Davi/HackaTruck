b42d0ecfee351d6d1a7e40e23a035e2f5726383a
Jonathan Davi <157547118+Jonathan-Davi@users.noreply.github.com>
2025-02-06 16:32:44 -0300

    Add files via upload

---
+++ Desafio01/Desafio01/Desafio01App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio01App.swift
+//  Desafio01
+//
+//  Created by Turma02-8 on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
